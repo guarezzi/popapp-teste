fbc49d1c4884909e54dc8ceb4bf16305015068c9
Luiz Ricardo Guarezzi <Luiz Ricardo Guarezzi>
2016-08-09 11:45:44 -0300

    Adicionado uma lista de contatos 'global' e alterado a funcionalidade de busca para procurar nesta lista se a primeira lista não trouxer resultados. Adicionado também uma mensagem para quando a busca não encontrar contatos em ambas as listas

--- www/js/Modules/contact/module.js
+++ www/js/Modules/contact/module.js
@@ -3,12 +3,21 @@
   
   function ContactCtrl($scope) {
     $scope.search = undefined;
-    $scope.persons = [
+    $scope.myContacts = [
       {name: 'Walter White', description: 'Back off, man. I\'m a scientist.'},
       {name: 'Teste AB', description: 'Back off, man. I\'m a scientist.'},
       {name: 'Teste CS', description: 'Back off, man. I\'m a scientist.'},
       {name: 'William Abbhram', description: 'Back off, man. I\'m a scientist.'},
       {name: 'Tasca J. Forei', description: 'Back off, man. I\'m a scientist.'}
+    ];
+
+    $scope.othersContacts = [
+      {name: 'Alan', description: 'Back off, man. I\'m a scientist.'},
+      {name: 'Giovanna', description: 'Back off, man. I\'m a scientist.'},
+      {name: 'Gary', description: 'Back off, man. I\'m a scientist.'},
+      {name: 'Sarah', description: 'Back off, man. I\'m a scientist.'},
+      {name: 'Aline', description: 'Back off, man. I\'m a scientist.'},
+      {name: 'July', description: 'Back off, man. I\'m a scientist.'}
     ];
   }
 
